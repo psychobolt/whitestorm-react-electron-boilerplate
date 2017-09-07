8f7e60c8a3e76e5ea8a5d3ff39025d337c618fbc
unknown <psychobolt@gmail.com>
2017-09-06 20:14:20 -0700

    include babel-polyfill in main webpack config

--- webpack.main.babel.js
+++ webpack.main.babel.js
@@ -6,7 +6,7 @@ import CleanWebpackPlugin from 'clean-webpack-plugin';
 import CommonConfig from './webpack.common';
 
 let config = {
-  entry: './src/main.js',
+  entry: ['babel-polyfill', './src/main.js'],
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'src', '.build'),
