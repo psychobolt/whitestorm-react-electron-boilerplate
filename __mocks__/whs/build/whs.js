73e200e9aca2573557a9da8b76d395730ff885a1
psychobolt <psychobolt@gmail.com>
2017-10-04 13:42:18 -0700

    fix stalling test

--- __mocks__/whs/build/whs.js
+++ __mocks__/whs/build/whs.js
@@ -1,11 +1,17 @@
 import * as WHS from 'whs/build/whs';
 
-export class MockModule {
+export class RenderingModule {
 }
 
-WHS.RenderingModule = MockModule;
-WHS.ResizeModule = MockModule;
-WHS.OrbitControlsModule = MockModule;
+export class ResizeModule {
+}
+
+export class OrbitControlsModule {
+}
+
+WHS.RenderingModule = RenderingModule;
+WHS.ResizeModule = ResizeModule;
+WHS.OrbitControlsModule = OrbitControlsModule;
 
 module.exports = {
   ...WHS,
