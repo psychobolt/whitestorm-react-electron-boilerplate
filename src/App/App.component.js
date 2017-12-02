eff803f9c0df400d3ed115fba24b72d508d4e0b4
psychobolt <psychobolt@gmail.com>
2017-12-01 18:25:18 -0800

    fix eslint style

--- src/App/App.component.js
+++ src/App/App.component.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import World from './World';
 import styles from './App.style';
 
-export default (props) => (
+export default props => (
   <div style={styles.container}>
     <div style={styles.header}><Header centered /></div>
     <World style={{ container: styles.worldContainer }} {...props} />
