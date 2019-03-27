23d7e617a05ba1c1e095d1cf52f5084e545eb26f
psychobolt <psychobolt@gmail.com>
2019-03-26 23:39:04 -0700

    Update history import

--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
-import createHistory from 'history/createMemoryHistory';
+import { createMemoryHistory } from 'history';
 
 import Routes from '../routes';
 
@@ -9,7 +9,7 @@ const mockStore = configureMockStore([]);
 
 test('component <Routes /> should render without crashing', () => {
   const props = {
-    history: createHistory({ keyLength: 0 }),
+    history: createMemoryHistory({ keyLength: 0 }),
     store: mockStore({}),
   };
   shallow(<Routes {...props} />);
